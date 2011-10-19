ceb16ebcba494c786bb2bff81acf1725e105bf01
Mike Chen <ccp0101@gmail.com>
2011-10-19 21:26:05 +0800

    Initial Commit

---
+++ iTransmission/ITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ITAppDelegate.h
+//  iTransmission
+//
+//  Created by Mike Chen on 10/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTransmission/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTransmission
+//
+//  Created by Mike Chen on 10/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ITAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ITAppDelegate class]));
+    }
+}
